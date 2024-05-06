c3571d6d68930b6fec45650294dbdf86a114e9d9
DVBDJ <163288149+DVBDJ@users.noreply.github.com>
2024-05-06 19:18:33 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+window.onload = function ExcuseGenerator() {
+    let randomwho = who[Math.floor(Math.random() * who.length)];
+    let randomaction = action[Math.floor(Math.random() * action.length)];
+    let randomwhat = what[Math.floor(Math.random() * what.length)];
+    let randomwhen = when[Math.floor(Math.random() * when.length)];
+    const excuse = ` ${randomwho} ${randomaction} ${randomwhat} ${randomwhen}`;
+    document.getElementById("excuse").innerHTML = excuse;
+  };
+
+
+
--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
+/* eslint-disable */
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "The mailman", "My bird", "My neighbor"];
+let action = ["ate", "peed", "crushed", "broke", "forgot"];
+let what = ["my homework", "my phone", "the car", "the keys"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+  "after dinner"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function ExcuseGenerator() {
+  let randomwho = who[Math.floor(Math.random() * who.length)];
+  let randomaction = action[Math.floor(Math.random() * action.length)];
+  let randomwhat = what[Math.floor(Math.random() * what.length)];
+  let randomwhen = when[Math.floor(Math.random() * when.length)];
+  const excuse = ` ${randomwho} ${randomaction} ${randomwhat} ${randomwhen}`;
+  document.getElementById("excuse").innerHTML = excuse;
 };
